fd7cbf273d07411421ae3f321f2255b1a2deb691
Dan Rose <dan.rose@wnco.com>
2019-05-26 15:32:40 -0500

    Full initial git push

---
+++ vendor/data_carr.js
@@ -0,0 +1,88 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-carr-keller-tx",
+      "name": "300ft Oakwood Circle, Keller TX",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.944344865200117,
+        "pitch": 0.5504129438654513,
+        "fov": 1.568238662786406
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.5109865692359996,
+          "pitch": 0.605874918819568,
+          "title": "Carr residence",
+          "text": "1021 Oakwood Circle, Keller TX"
+        }
+      ]
+    },
+    {
+      "id": "1-carr-keller-tx",
+      "name": "250ft Oakwood Circle, Keller TX",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.944344865200117,
+        "pitch": 0.5504129438654513,
+        "fov": 1.568238662786406
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+  ],
+  "name": "Panoramas",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
